80ad24dd7d60d98705fbe776bf5d98c70e3c5d06
Quarren42 <nicholas.junker125@gmail.com>
2015-05-13 19:07:33 -0400

    Fixed up the code
    fixed up the transmitter / receiver code - must be ran on two separate
    processors however.

--- 433Mhz_TX_RX/receiver/receiver.ino
+++ 433Mhz_TX_RX/receiver/receiver.ino
@@ -9,38 +9,17 @@
 // $Id: receiver.pde,v 1.3 2009/03/30 00:07:24 mikem Exp $
 
 #include <VirtualWire.h>
-#include <Wire.h>
-#include <Adafruit_MotorShield.h>
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-
-const int led_pin = 6;
-//const int transmit_pin = 3;
-const int receive_pin = 5;
-//const int transmit_en_pin = 2;
-const int motor = 7;
-
-boolean msgToggle;
+const int receive_pin = 4;
 
 void setup()
 {
   delay(1000);
   Serial.begin(9600);	// Debugging only
   Serial.println("setup");
-  AFMS.begin();
-  
-  // Initialise the IO and ISR
-  
   vw_set_rx_pin(receive_pin);
   vw_setup(4000);	 // Bits per sec
   vw_rx_start();       // Start the receiver PLL running
-  
-  pinMode(led_pin, OUTPUT);
-  pinMode(motor, OUTPUT);
-  
-  myMotor -> setSpeed(255);
 }
 
 void loop()
@@ -50,40 +29,14 @@ void loop()
 
   if (vw_get_message(buf, &buflen)) // Non-blocking
   {
+    Serial.print("Got: ");
 
-    digitalWrite(led_pin, HIGH); // Flash a light to show received good message
-    // Message with a good checksum received, dump it.
-
-    if (msgToggle == false){
-      if (buf[0] == 'O'){
-        if (buf[1] == 'N'){
-          Serial.println("MOVEMENT DETECTED");
-          msgToggle = true;
-        }
-      }
+    for (int i = 0; i < buflen; i++)
+    {
+      Serial.print(buf[i], HEX);
+      Serial.print(' ');
     }
-
-    if (msgToggle == true){
-      if (buf[0] == 'O'){
-        if (buf[1] == 'F'){
-          if (buf[2] == 'F'){
-            Serial.println("NO MOVEMENT DETECTED");
-            msgToggle = false;
-          }
-        }
-      }
-    }
-    digitalWrite(led_pin, LOW);
-  }
-
-  if (msgToggle == true){
-    myMotor -> run(FORWARD);
-  }
-  else {
-    myMotor -> run(RELEASE);
+    Serial.println();
   }
 }
-
-
-
 
--- 433Mhz_TX_RX/transmitter/transmitter.ino
+++ 433Mhz_TX_RX/transmitter/transmitter.ino
@@ -10,43 +10,34 @@
 
 #include <VirtualWire.h>
 
-const int led_pin = 3;
-const int transmit_pin = 5;
-const int IR_pin = 2;
-//const int receive_pin = 2;
-//const int transmit_en_pin = 6;
+const int led_pin = 4;
+const int transmit_pin = 6;
 
 void setup()
 {
     // Initialise the IO and ISR
     vw_set_tx_pin(transmit_pin);
-  //  vw_set_ptt_inverted(true); // Required for DR3100
-    vw_setup(4000);       // Bits per sec
     pinMode(led_pin, OUTPUT);
-    pinMode(IR_pin, INPUT);
+    
+    vw_setup(4000);       // Bits per sec
+    vw_rx_start();//
     Serial.begin(9600);
 }
 
-byte count = 1;
-
 void loop()
 {
+  
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
   char msg[2] = {'O','N'};
   
-  if (digitalRead(IR_pin) == HIGH){
-    
-  digitalWrite(led_pin, LOW); // Flash a light to show transmitting
+  digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
   vw_send((uint8_t *)msg, 2);
   vw_wait_tx(); // Wait until the whole message is gone
-  Serial.println("ON");
-  digitalWrite(led_pin, HIGH);
-  
-  }
-  else {
-    char msg[3] = {'O','F','F'};
-    vw_send((uint8_t *)msg, 3);
-    vw_wait_tx();
-    Serial.println("OFF");
-    digitalWrite(led_pin, LOW);
-  }
+  digitalWrite(led_pin, LOW);
+ delay(150);
 }
+
+  
+
